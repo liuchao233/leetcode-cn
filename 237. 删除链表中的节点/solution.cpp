85a402134b14f8fa0ec10c3cb7d6875c140205e0
liuchao <chao.c.liu@capgemini.com>
2021-08-23 11:54:30 +0800

    237. 删除链表中的节点

---
+++ 237. 删除链表中的节点/solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* next = node->next;
+        node->val = next->val;
+        node->next = next->next;    
+        next->next = NULL;    
+    }
+};
