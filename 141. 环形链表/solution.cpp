8d8c52c0d9358601e5975bfd0082ce12a3c15c8c
liuchao233 <15609831969@163.com>
2021-08-25 12:11:00 +0800

    141. 环形链表

---
+++ 141. 环形链表/solution.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL) {
+            return false;
+        }
+        ListNode *fast = head, *slow = head;
+
+        while (fast->next != NULL && fast->next->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
