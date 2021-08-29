9462a19bcf35dcfc309a3971944fdd90a6eaf8b4
liuchao233 <15609831969@163.com>
2021-08-29 22:37:04 +0800

    27. 移除元素

--- 27. 移除元素/solution.cpp
+++ 27. 移除元素/solution.cpp
@@ -5,6 +5,20 @@ using namespace std;
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) {
+                ++count;
+            }
+        }
 
+        for (int i = 0, j = 0; i < nums.size() && j < count; ++i) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        return count;
     }
 };
